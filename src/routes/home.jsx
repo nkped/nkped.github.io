19917b04c88c6ddbe62d1837900818dd5f1097ae
nkped <nikrpedev@gmail.com>
2022-05-12 19:46:56 +0200

    Tilføj Footer og tilbage til grid i Home

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,40 +1,30 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { Footer } from "../components/footer";
 
 export default function Home() {
   return (
-    <Container>
-      <Row>
+    <Container className="h-600px border border-primary">
+      <Container className="pt-3">
         <h3>Hej, Nils-Kristian her!</h3>
-      </Row>
-      <Container className="mt-3">
+      </Container>
+      <Container className="px-5 pt-3">
         <h2>Jeg laver frontend-udvikling, og søger en praktikplads.</h2>
       </Container>
-      <Container //className="d-grid gap-5 mt-5"
-      >
+      <Button variant="primary" className="w-25 mx-auto my-4">
+        Kontakt
+      </Button>
+      <Container className="d-grid gap-5 border border-primary">
         <Row>
-          <Button variant="primary" className="w-25 mx-auto my-4">
-            Kontakt
-          </Button>
-        </Row>
-        <Row className="mb-3">
-          <Col>
-            <h4>Voksen</h4>
-          </Col>
-          <Col></Col>
-          <Col></Col>
-        </Row>
-        <Row className="mb-5">
-          <Col>
-            <h3>Selv-lærende</h3>
-          </Col>
+          <Col className="border text-start">Voksen</Col>
         </Row>
         <Row>
-          <Col></Col>
-          <Col>
-            <h1>Ambitiøs..</h1>
-          </Col>
+          <Col className="border">Selv-lærende</Col>
+        </Row>
+        <Row>
+          <Col className="text-end">Ambitiøs</Col>
         </Row>
       </Container>
+      <Footer />
     </Container>
   );
 }
--- src/routes/om.jsx
+++ src/routes/om.jsx
@@ -1,7 +1,35 @@
+import { Container } from "react-bootstrap";
+
 export default function Om() {
   return (
-    <main>
-      <h2>Du er på Om!</h2>
-    </main>
+    <Container>
+      <Container className="d-flex">
+        <h1>Om</h1>
+      </Container>
+      <Container>
+        <p>
+          Jeg er selv-lærende frontend-udvikler, og jeg ønsker at dyggtigøre mig
+          professionelt under en praktik. Jeg er uddannet i journalistik og
+          samfundsvidenskab fra Roskilde UniversitetCenter, og har mange gode
+          erfaringer i bagagen som leverandør på contenr siden af nettet. BDa
+          Corona brød løs og hele Danmark tog en dyb kollektiv indånding gjorde
+          jeg det samme. Jeg har studeret frontend-udvikling i henved to år, og
+          mener nu, jeg er klar på en praktik.
+        </p>
+        <Container className="d-flex">
+          <h3>Jeg bidrager med..</h3>
+        </Container>
+        <p>Jeg ved, jeg har masser at lære - og glæder mig til det!</p>
+        <p>
+          Når det er sagt, har jeg efterhånden overvundet så mange faglige
+          udfordringer, at min selvtillid er temmelig god. Jeg forventer at
+          blive overvældet i min praktik, men også, at få styr på det.
+        </p>
+        <p>
+          Jeg gør altid mit bedste for at være en sjov, empatisk og
+          samvittighedsfuld kollega.{" "}
+        </p>
+      </Container>
+    </Container>
   );
 }
