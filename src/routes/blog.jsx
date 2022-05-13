1d37612c55a2910a179fed10ddce777106145f4a
nkped <nikrpedev@gmail.com>
2022-05-13 17:43:11 +0200

    Tilføj ekstra data

--- src/routes/blog.jsx
+++ src/routes/blog.jsx
@@ -1,22 +1,36 @@
 import { Outlet } from "react-router-dom";
 import { getAllBlogPosts } from "../data";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Blog() {
   let blogPosts = getAllBlogPosts();
   return (
-    <div>
+    <Container>
       <Outlet />
-      <div>
-        <h3>Seneste blogposts:</h3>
-      </div>
-      <nav>
-        {blogPosts.map((blogPost) => (
-          <Link to={`/blog/${blogPost.number}`} key={blogPost.number}>
-            {blogPost.titel}
-          </Link>
-        ))}
-      </nav>
-    </div>
+      <Container className="text-start">
+        <h3>Blogposts:</h3>
+      </Container>
+      <Container className="p-4">
+        <Container>
+          {blogPosts.map((blogPost) => {
+            return (
+              <Row>
+                <Col className="pb-3">
+                  {`${blogPost.subject}: `}
+                  <Link
+                    className="text-decoration-none"
+                    to={`/blog/${blogPost.number}`}
+                    key={blogPost.number}
+                  >
+                    {blogPost.titel}
+                  </Link>
+                </Col>
+              </Row>
+            );
+          })}
+        </Container>
+      </Container>
+    </Container>
   );
 }
