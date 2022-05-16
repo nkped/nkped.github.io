9651df06fdc50dfd0b59dd20682dbbeb38ebe677
nkped <nikrpedev@gmail.com>
2022-05-16 16:16:58 +0200

    Tilføj svg til Home, plus oprettet img dir

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,17 +1,20 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import { Footer } from "../components/footer";
+//import homePagePic from "../img/homePagePic.png";
+import HomePageImage from "./HomePageImage.svg";
 
 export default function Home() {
   return (
     <Container fluid className="bg-dark text-light">
       <Container className="pt-3">
-        <h3>Hej, Nils-Kristian her!</h3>
+        <h4>Hej, Nils-Kristian her!</h4>
       </Container>
       <Container className="px-5 pt-3">
         <h2>
-          Jeg laver frontend-udvikling, og{" "}
-          <span className="text-warning ">søger en praktikplads.</span>
+          Jeg laver frontend-udvikling, og søger en{" "}
+          <span className="text-warning ">praktikplads.</span>
         </h2>
+        <img src={HomePageImage} />
       </Container>
       <Button variant="primary" className="w-25 mx-auto my-4">
         Kontakt
