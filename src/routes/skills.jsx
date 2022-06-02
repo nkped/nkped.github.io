cdb682b54aa2c71300e7f85a9417b8264c074be6
nkped <nikrpedev@gmail.com>
2022-06-02 20:45:29 +0200

    Alle iconer plus tekst lagt på Skills

--- src/routes/skills.jsx
+++ src/routes/skills.jsx
@@ -10,14 +10,43 @@ import {
   FaCss3,
   FaHtml5,
   FaLinux,
+  FaRegGrinWink,
 } from "react-icons/fa";
 import { DiVisualstudio } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiLighthouse } from "react-icons/si";
 import { Footer } from "../components/footer";
 
 export default function Skills() {
   return (
     <Container className="bg-dark text-light">
-      <Container border-bottom border-warning className="d-grid gap-3">
+      <Container className="fs-1 fw-semibold mb-2 pb-1 text-muted  border-bottom border-warning">
+        <Row>Skills</Row>
+      </Container>
+      <Container
+        border-bottom
+        border-warning
+        className="d-grid gap-3 py-4 border-bottom border-warning"
+      >
+        <Row>
+          <Col>
+            Her er min nuværende 'tech-stack' beskrevet - rub og stub er med.{" "}
+            <FaRegGrinWink />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Jeg er selvfølgelig ikke ekspert, men habil i basics, og super
+            nysgerrig på at udvikle mig.
+          </Col>
+        </Row>
+        <Row className="mb-4">
+          <Col>
+            {" "}
+            Mine næste mål er anført aller nederst på siden, og tæller derfor
+            ikke som skills for nuværende.
+          </Col>
+        </Row>
         <Row>
           <Col>
             <Card className="bg-secondary">
@@ -113,6 +142,38 @@ export default function Skills() {
             </Card>
           </Col>
         </Row>{" "}
+        <Row className="fs-5 text-center mt-5 mb-2">
+          <Col>Min målsætning fremover</Col>
+        </Row>
+        <Row>
+          <Col>
+            Jeg er solgt på ideen om PWA's, der lever på nettet fremfor i
+            AppStore/GooglePlay, og som virker på alle platforme med blot een
+            codebase.
+          </Col>
+          <Row>
+            <Col>De næste skills må derfor være test samt server-side.</Col>
+          </Row>
+        </Row>
+        <Row>
+          <Col>
+            <Card className="bg-secondary">
+              <Card.Body>
+                <SiLighthouse />
+                <Card.Title>Lighthouse</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card className="bg-secondary">
+              <Card.Body>
+                <TbBrandNextjs />
+                <Card.Title>NextJS</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
       <Footer />
     </Container>
