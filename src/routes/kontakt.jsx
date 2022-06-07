52437826608bc7137a8a841334352598bbcc6e2e
nkped <nikrpedev@gmail.com>
2022-06-07 12:26:54 +0200

    Tilføj stack på Info

--- src/routes/kontakt.jsx
+++ src/routes/kontakt.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Stack } from "react-bootstrap";
 import { Footer } from "../components/footer";
 import { Telephone, Envelope, Github, Linkedin } from "react-bootstrap-icons";
 
@@ -8,106 +8,117 @@ export default function Kontakt() {
       <Container className="fs-1 fw-semibold mb-2 pb-1 text-muted  border-bottom border-warning">
         <Row>Info</Row>
       </Container>
-      <Container className="d-grid gap-3 text-start px-4 py-5">
+      <Container className="text-start py-5">
         <Row className="fs-3 fw-bold text-start mb-2">
-          <Col>Vilkår for praktik</Col>
+          <Col md={2} lg={3}></Col>
+          <Col md={8} lg={6}>
+            Vilkår for praktik
+          </Col>
+          <Col md={2} lg={3}></Col>
         </Row>
         <Row>
-          <Col>
-            <span className="lead text-warning">Gratis </span>- virksomheden
-            skal blot sørge for frokost og kontorplads.
+          <Col md={2} lg={3}></Col>
+          <Col md={8} lg={6}>
+            <p>
+              <span className="lead text-warning">Gratis </span>- virksomheden
+              skal blot sørge for frokost og kontorplads.
+            </p>
+            <p>
+              <span className="lead text-warning">Formålet </span>med praktik er
+              primært joberfaring - ikke nødvendigvis job i samme virksomhed
+              efterfølgende.
+            </p>
+            <p>
+              <span className="lead text-warning">Varighed </span>for praktik er
+              i udgangspunktet tre måneder.
+            </p>
+            <p>
+              <span className="lead text-warning">Min rolle</span> som
+              praktikant skal selvfølgelig primært udvikle mig som programmør.
+              Jeg er dog åben overfor at bidrage på andre områder for, at det
+              giver mening for virksomheden at have et føl gående.
+            </p>
+            <p>
+              <span className="lead text-warning"> Jeg har erfaring</span> som
+              primær website-skribent for founthouse.dk, mødebooker for Nerd
+              Productions samt manusforfatter på engelsk-sproglig pr-video for
+              Citizen Dane (se referencer nederst).
+            </p>
+            <p>
+              <span className="lead text-warning">Skriv eller ring</span> på
+              nedenstående info.
+            </p>
           </Col>
+          <Col md={2} lg={3}></Col>
         </Row>
-        <Row>
-          <Col>
-            <span className="lead text-warning">Formålet </span>med praktik er
-            primært joberfaring - ikke nødvendigvis job i samme virksomhed
-            efterfølgende.
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <span className="lead text-warning">Varighed </span>for praktik er i
-            udgangspunktet tre måneder.
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <span className="lead text-warning">Min rolle</span> som praktikant
-            skal selvfølgelig primært udvikle mig som programmør. Jeg er dog
-            åben overfor at bidrage på andre områder for, at det giver mening
-            for virksomheden at have et føl gående.
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            Jeg har fx været primær website-skribent for founthouse.dk,
-            mødebooker for Nerd Productions samt manusforfatter på
-            engelsk-sproglig pr-video for Citizen Dane (se referencer nederst).
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <span className="lead text-warning">Skriv eller ring</span> på
-            nedenstående info.
-          </Col>
-        </Row>
-      </Container>
-      <Container className="w-75 px-5 pb-5 border-bottom border-warning">
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Telephone color="orange" size={40} className=" mb-3" />
-            <Card.Title>6088 7055</Card.Title>
-          </Card.Body>
-        </Card>
+        <Stack
+          gap={4}
+          className="col-8 col-sm-7 col-md-5 col-lg-4 mx-auto mt-5 text-center"
+        >
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Telephone color="orange" size={40} className=" mb-3" />
+              <Card.Title>6088 7055</Card.Title>
+            </Card.Body>
+          </Card>
 
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Envelope color="orange" size={40} className=" mb-3" />
-            <Card.Title>nikrpe@gmail.com</Card.Title>
-          </Card.Body>
-        </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Envelope color="orange" size={40} className=" mb-3" />
+              <Card.Title>nikrpe@gmail.com</Card.Title>
+            </Card.Body>
+          </Card>
 
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Github color="orange" size={40} className=" mb-3" />
-            <Card.Title>nikrpedev@gmail.com</Card.Title>
-          </Card.Body>
-        </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Github color="orange" size={40} className=" mb-3" />
+              <Card.Title>nikrpedev@gmail.com</Card.Title>
+            </Card.Body>
+          </Card>
 
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Linkedin color="orange" size={40} className=" mb-3" />
-            <Card.Title>Nils-Kristian Pedersen</Card.Title>
-          </Card.Body>
-        </Card>
-        <Row>
-          <Col className="fs-3">Referencer</Col>
-        </Row>
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Telephone color="orange" size={40} className=" mb-3" />
-            <Card.Title>Founthouse.dk</Card.Title>
-            <Card.Text>Toke Schultze: 5050 9015</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Telephone color="orange" size={40} className=" mb-3" />
-            <Card.Title>Citizen Dane</Card.Title>
-            <Card.Text>Marcus Mandal: 3363 7302</Card.Text>
-          </Card.Body>
-        </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Linkedin color="orange" size={40} className=" mb-3" />
+              <Card.Title>Nils-Kristian Pedersen</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Telephone color="orange" size={40} className=" mb-3" />
+              <Card.Title>Founthouse.dk</Card.Title>
+              <Card.Title>Reference:</Card.Title>
+              <Card.Text>Toke Schultze: 5050 9015</Card.Text>
+              <Card.Text>Stilling: Ejer</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Telephone color="orange" size={40} className=" mb-3" />
+              <Card.Title>Citizen Dane</Card.Title>
+              <Card.Title>Reference:</Card.Title>
+              <Card.Text>Marcus Mandal: 3363 7302</Card.Text>
+              <Card.Text>Stilling: Instruktør</Card.Text>
+            </Card.Body>
+          </Card>
 
-        <Card className="my-4 bg-secondary">
-          <Card.Body>
-            <Telephone color="orange" size={40} className=" mb-3" />
-            <Card.Title>Nerd Productions</Card.Title>
-            <Card.Text>Bo Blaksteen: 2299 4253</Card.Text>
-          </Card.Body>
-        </Card>
+          <Card className="bg-secondary">
+            <Card.Body>
+              <Telephone color="orange" size={40} className=" mb-3" />
+              <Card.Title>Nerd Productions</Card.Title>
+              <Card.Title>Reference:</Card.Title>
+              <Card.Text>Bo Blaksteen: 2299 4253</Card.Text>
+              <Card.Text>Stilling: Ejer</Card.Text>
+            </Card.Body>
+          </Card>
+        </Stack>
       </Container>
       <Footer />
     </Container>
   );
 }
+
+/*
+<Container className="w-75 px-5 pb-5 border-bottom border-warning"></Container>
+<Col className="fs-3">Referencer</Col>
+
+*/
