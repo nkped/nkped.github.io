61ca3ac8ab3c4edfa2d8124991c9796c3e8088e5
nkped <nikrpedev@gmail.com>
2022-06-08 17:09:17 +0200

    Text på Om responsive

--- src/routes/om.jsx
+++ src/routes/om.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Stack } from "react-bootstrap";
 import { Footer } from "../components/footer";
 
 export default function Om() {
@@ -7,38 +7,46 @@ export default function Om() {
       <Container className="border-bottom border-warning fs-1 fw-semibold mb-4 text-muted">
         <Row>Om</Row>
       </Container>
-      <Container className=" text-start p-5">
+      <Stack gap={4} className="pt-5 text-start">
         <Row>
-          <Col md={2} lg={3}></Col>
-          <Col md={8} lg={6}>
-            <Col className="lead">
-              Selv-lærende frontend-udvikler, der ønsker at dyggtigøre sig
-              professionelt under en praktik.
-            </Col>
-            <Col>
-              Jeg er uddannet i journalistik og samfundsvidenskab på Roskilde
-              Universitet, og har mange gode erfaringer i bagagen som leverandør
-              af web-content, både journalistik, hjemmeside-tekst samt pr-video.
-            </Col>
-            <Col>
-              Da Corona brød løs og hele Danmark tog en dyb kollektiv indånding
-              gjorde jeg det samme. Siden da, har jeg studeret
-              frontend-udvikling på fuld tid, og mener nu, jeg er klar på en
-              praktik.
-            </Col>
-            <Col>Jeg ved, jeg har masser at lære - og glæder mig til det!</Col>
-            <Col>
-              Når det er sagt, har jeg efterhånden overvundet så mange faglige
-              udfordringer, at min selvtillid er robust - jeg forventer at få
-              hovedbrud i min praktik, men også at få styr på det.
-            </Col>
+          <Col className="lead mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+            Selv-lærende frontend-udvikler, der ønsker at dyggtigøre sig
+            professionelt under en praktik.
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+            Jeg er uddannet i journalistik og samfundsvidenskab på Roskilde
+            Universitet, og har mange gode erfaringer i bagagen som leverandør
+            af web-content, både journalistik, hjemmeside-tekst samt pr-video.
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+            Da Corona brød løs og hele Danmark tog en dyb kollektiv indånding
+            gjorde jeg det samme. Siden da, har jeg studeret frontend-udvikling
+            på fuld tid, og mener nu, jeg er klar på en praktik.
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+            Jeg ved, jeg har masser at lære - og glæder mig til det!
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+            Når det er sagt, har jeg efterhånden overvundet så mange faglige
+            udfordringer, at min selvtillid er robust - jeg forventer at få
+            hovedbrud i min praktik, men også at få styr på det.
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto col-10 col-sm-9 col-md-7 col-lg-6 col-xl-5">
             Jeg gør altid mit bedste for at være en sjov, empatisk og
             samvittighedsfuld kollega.
-            <Col></Col>
           </Col>
-          <Col md={2} lg={3}></Col>
         </Row>
-      </Container>
+      </Stack>
       <Footer />
     </Container>
   );
