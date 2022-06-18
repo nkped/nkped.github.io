dfd197ec180a8818ba0d0afe468644bcb70864a9
nkped <nikrpedev@gmail.com>
2022-06-18 17:49:52 +0200

    Tilføj prefers-color-scheme

--- src/routes/blogLandingPage.jsx
+++ src/routes/blogLandingPage.jsx
@@ -4,15 +4,23 @@ import { Footer } from "../components/footer";
 
 export default function BlogLandingPage() {
   return (
-    <Container fluid className="montFont">
+    <Container fluid className="nFont">
       <Container className="fs-1 fw-semibold mb-2 text-muted text-start">
-        <Row>Blog</Row>
+        <Row>
+          <h1>Blog</h1>
+        </Row>
       </Container>
-      <Container className="text-center px-4 py-3 border-top border-warning">
-        <Card className="my-4 bg-dark">
+      <Container className="text-center px-4 py-3">
+        <Card
+          className="my-4
+        
+         bg-dark border border-warning"
+        >
           <Card.Body>
             <Pen color="orange" size={40} className=" mb-5" />
-            <Card.Title>Velkommen til min blog</Card.Title>
+            <Card.Title>
+              <h2>Velkommen til min blog</h2>
+            </Card.Title>
             <Card.Text>
               Her øver jeg mig i at dokumentere ved at skrive manualer jeg
               hutigt kan vende tilbage til.
