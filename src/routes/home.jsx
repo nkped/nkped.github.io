85dbbf0c0cb1b37e94960d65a4086a066c5e3ef4
nkped <nikrpedev@gmail.com>
2022-06-20 21:36:50 +0200

    Ændringer på Home

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -8,33 +8,29 @@ import { ArrowRight } from "react-bootstrap-icons";
 
 export default function Home() {
   return (
-    <Container fluid className="bg-dark text-light">
-    <Row><Col>
-          <Image fluid src={YellowHomePageImage} />
-          </Col> </Row>
-        <Row>
-          <Col>
-            <h1 className="Noto700 text-warning">
-              Frontend-udvikler søger{" "}
-              praktikplads
-            </h1>
-          </Col>
-        </Row>
-
-
-        <Button href="/#/kontakt" variant="primary" className="Noto600 my-4">
-          Kontakt og info
-          <ArrowRight className="ms-1" />
-        </Button>
-          <Row  className="text-center">
-            <Col>
-              <p>Venlig hilsen</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col><p>Nils-Kristian Pedersen</p></Col>
-          </Row>
-          <Footer />
+    <Container className="bg-dark text-light">
+      <Row className="skewed">
+        <h1 className="Noto700 text-warning display-2">
+          Frontend-udvikler søger praktikplads
+        </h1>
+      </Row>
+      <Row>
+        <Col xs={{ span: 6, offset: 5 }}>..vh Nils-Kristian</Col>{" "}
+      </Row>
+      <Button href="/#/kontakt" variant="primary" className="Noto600 my-4">
+        Kontakt og info
+        <ArrowRight className="ms-1" />
+      </Button>
+      <Footer />
     </Container>
   );
 }
+
+/*
+      <Row>
+        <Col xs={{ span: 6, offset: 3 }}>
+          <p>..mvh Nils-Kristian Pedersen</p>
+        </Col>
+      </Row>
+
+      */
