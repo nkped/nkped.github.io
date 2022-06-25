1c2570471edb225f31937f0ba744d7e2cedb3e00
nkped <nikrpedev@gmail.com>
2022-06-25 16:45:34 +0200

    Rettet Om

--- src/routes/om.jsx
+++ src/routes/om.jsx
@@ -15,6 +15,7 @@ export default function Om() {
         <Row className="my-5 justify-content-center">
           <Col
             xs={{ span: 10, offset: 0 }}
+            lg={{ span: 6, offset: 0 }}
             className="fs-2 fst-italic text-align-center mt-2 mb-3 "
           >
             <p>
@@ -23,15 +24,23 @@ export default function Om() {
             </p>
           </Col>
           <Col xs={{ span: 10, offset: 0 }}>
-            <FaHandsHelping />
+            <FaHandsHelping size={80} />
           </Col>
         </Row>
 
-        <Row className="bgBuddha mb-5 align-items-end">
-          <Col xs={{ span: 10, offset: 1 }} className="Noto700 fs-2 mt-5 pt-5">
+        <Row className="bgBuddha mb-5 text-start">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="Noto600 fs-2 mt-5 pt-5"
+          >
             Zen-beslutning, to år efter..
           </Col>
-          <Col xs={{ span: 10, offset: 1 }} className="mb-0 pb-3">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="mb-5 pb-5"
+          >
             <p>
               To års studier i web-udvkling er fløjet afsted, efter Corona
               sendte det kollektive Danmark på tvungen selvrefleksion i 2020.
@@ -39,11 +48,19 @@ export default function Om() {
           </Col>
         </Row>
 
-        <Row className="bgPaperBoy mb-5 align-items-end">
-          <Col xs={{ span: 10, offset: 1 }} className="Noto700 fs-2 mt-3 pt-5">
+        <Row className="mb-5 text-start">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="Noto600 fs-2 mt-3 pt-5"
+          >
             Uddannet i journalistik, RUC
           </Col>
-          <Col xs={{ span: 10, offset: 1 }} className="mb-0 pb-3">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="mb-0 pb-3"
+          >
             <p>
               {" "}
               Gode erfaringer i bagagen som leverandør af web-content, både
@@ -52,32 +69,46 @@ export default function Om() {
           </Col>
         </Row>
 
-        <Row className="bgAstronaut mb-5 align-items-end">
-          <Col xs={{ span: 10, offset: 1 }} className="Noto700 fs-2 mt-4 pt-0">
-            The Truth Is Out There!
-          </Col>
-          <Col xs={{ span: 10, offset: 1 }}>
+        <Row className="mb-5 text-start">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="Creepster display-2 mt-5"
+          >
+            The Truth Is Out There!{" "}
             <RiAliensLine style={{ color: "green" }} size={80} />
           </Col>
-          <Col xs={{ span: 10, offset: 1 }} className="mb-0 pb-0"></Col>
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="text-start"
+          >
+            Fan af science og sci-fi.
+          </Col>
         </Row>
-        <Row className="bgTreasure mb-5 align-items-end">
-          <Col xs={{ span: 10, offset: 1 }} className="Noto700 fs-2  mt-5 pt-0">
+        <Row className="mb-5 text-start">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="Noto600 fs-2  mt-5 pt-0"
+          >
             Skattejagts-forfatter
           </Col>
-          <Col xs={{ span: 10, offset: 1 }} className="mb-0 pb-3">
+          <Col xs={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
             <p>
               Min datter - en gæv 12-årig - må udholde sin fars trang til
               arts&crafts og let teater på sine fødselsdage. Det må vennernes
-              børn , og endda et par voksne i familien.
+              børn også, og endda et par voksne i familien.
             </p>
           </Col>
-          <Col xs={{ span: 10, offset: 1 }}>
-            <BsEmojiSmileUpsideDown size={40} />
-          </Col>
+          <Col xs={{ span: 10, offset: 1 }} className="bgTreasure mb-5"></Col>
         </Row>
         <Row className="mt-5 py-3">
-          <Col xs={{ span: 10, offset: 1 }} className="fs-2 fst-italic">
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            lg={{ span: 6, offset: 3 }}
+            className="fs-2 fst-italic"
+          >
             "Jeg gør altid mit bedste for at være en sjov, empatisk og
             samvittighedsfuld kollega."
           </Col>
