101eecef8ff0408ec32f6995e6ad0a0c89156990
nkped <nikrpedev@gmail.com>
2022-06-27 14:50:42 +0200

    Rettet BlogLayout

--- src/routes/blogLandingPage.jsx
+++ src/routes/blogLandingPage.jsx
@@ -8,34 +8,39 @@ export default function BlogLandingPage() {
       <Row className="display-2 Noto700 pb-1 px-1 mb-2 text-muted  border-bottom border-warning">
         Blog
       </Row>
-      <Row className="text-center pt-3">
-        <Card className="my-4 bg-dark">
-          <Card.Body className="p-4">
-            <Card.Title>
-              <h1 className="Noto700">nkp:NoGobledigook</h1>
-            </Card.Title>
-            <Pen color="orange" size={40} className=" mt-3 mb-5" />
-            <Card.Text className="">
-              Her skriver jeg manualer samt om tips og tricks inden for
-              webudvikling.
-            </Card.Text>
-            <Card.Text>
-              Jeg arbejder med
-              <span className="text-warning">ReactJS</span> og{" "}
-              <span className="text-warning ">React-Bootstrap</span>.
-            </Card.Text>
-            <Card.Text>
-              Inden længe kan man filtrere blogindlæg på emner - fx "routing",
-              via knapper.
-            </Card.Text>
-            <Card.Text>
-              Indtil videre kan man kun vælge "alle indlæg" nedenfor.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+
+      <Row className="text-center mt-5 pt-2">
+        <Col>
+          <h1 className="Noto700">nkp:NoGobledigook</h1>
+        </Col>
+      </Row>
+      <Row className=" mt-4 mb-5">
+        <Col>
+          <Pen color="orange" size={40} />
+        </Col>
+      </Row>
+      <Row className=" mb-4">
+        <Col xs={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
+          Her skriver jeg manualer samt om tips og tricks inden for
+          webudvikling.
+        </Col>
+      </Row>
+      <Row className=" mb-4">
+        <Col xs={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
+          Jeg udvikler inden for <span className="text-warning">ReactJS</span>{" "}
+          og <span className="text-warning ">React-Bootstrap</span>, men skriver
+          bredt om økosystemet.
+        </Col>
       </Row>
       <Row>
-        <Col className="display-2 text-center Noto700 mb-4">#</Col>
+        <Col xs={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
+          Inden længe kan man filtrere blogindlæg på emner - fx "routing", via
+          knapper. Indtil videre kan man kun vælge "alle indlæg" nedenfor.
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="display-2 text-center Noto700 my-2">#</Col>
       </Row>
       <Row>
         <Col>
