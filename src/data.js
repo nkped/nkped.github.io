0b577b0b30011e2a7142bbccc408a8352c73bfc9
nkped <nikrpedev@gmail.com>
2022-06-29 11:49:50 +0200

    data.js update

--- src/data.js
+++ src/data.js
@@ -49,6 +49,15 @@ let blogPosts = [
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     author: "Nils-Kristian Pedersen",
   },
+  {
+    number: 55,
+    subject: "",
+    titel: "",
+    intro: "",
+    image: "",
+    text: "",
+    author: "Nils-Kristian Pedersen",
+  },
 ];
 
 export function getAllBlogPosts() {
